b4d7da5fead626e612fc9a89d7ec7188fdb2f956
rockstarapesmint <107557832+rockstarapesmint@users.noreply.github.com>
2022-06-15 16:30:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7219,7 +7219,7 @@ var account = null;
 
         const connectWallet = async () =>{
         if(window.ethereum){
-            await window.ethereum.send('eth_requestAccounts');
+            await ethereum.request({method: "eth_requestAccounts"});
             window.web3 = new Web3(window.ethereum);
 
             var accounts = await web3.eth.getAccounts();
